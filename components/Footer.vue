4ff15c6c22783fc6264e024e95cae1fc5e84f30e
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2020-10-13 10:55:39 -0700

    Fixing Spelling mistake

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <footer class="text-center bg-gray-800 text-white">
       <h2>&copy;Hi5 Studio</h2>
       <h1>
-        Disclaimer: This is not the offical Hi5 Studio website. You can find the official one <a class="text-blue" href="https://hi5studios.com">here</a>. This is just a fun project I want to do.
+        Disclaimer: This is not the official Hi5 Studio website. You can find the official one <a class="text-blue" href="https://hi5studios.com">here</a>. This is just a fun project I want to do.
         I am not claiming anything you see on this website as mine. All credit goes to Hi5 Studio.
         They have the power to ask for this to be remove, and it will happen.
         This also means that I can't put the down members of Hi5 Studio, and pictures of them without consent, which I don't have.
